b4dbef5d4068835cd49d8bcd18a4a1a0509cbe57
tykavanaugh <tykavanaugh@gmail.com>
2022-05-07 16:25:45 -0700

    torus

--- fireship3d/main.js
+++ fireship3d/main.js
@@ -1,6 +1,37 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+camera.position.setZ(30);
+renderer.render(scene,camera);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100);
+const material = new THREE.MeshBasicMaterial({color:0xFF6347,wireframe:true});
+const torus = new THREE.Mesh(geometry,material)
+
+// const pointLight = new THREE.PointLight(0xffffff);
+// pointLight.position.set(5, 5, 5);
+
+// const ambientLight = new THREE.AmbientLight(0xffffff);
+// scene.add(pointLight, ambientLight);
+
+scene.add(torus)
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += .01;
+  torus.rotation.y += .005;
+  torus.rotation.z += .01;
+
+  renderer.render(scene,camera);
+}
+
+animate()
